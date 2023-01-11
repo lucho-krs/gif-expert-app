4ca286d934b982f2d05d8510ce7bf3f256d25f4c
Luis Cereño <lrcereno@pop-os.localdomain>
2023-01-11 14:00:15 -0300

    Se agrega estilo al GifExpertApp

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,12 @@
 export const GifExpertApp = () => {
   return (
     <>
+        {/* Titulo */}
         <h1> GifExpertApp! </h1>
+
+        {/* Input */}
+
+        {/* Listado de Gif */}
     </>
   )
 }
