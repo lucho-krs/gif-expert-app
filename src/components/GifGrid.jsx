74c1af036206d8d45296098ab0ee452656857785
Luis Cereño <lrcereno@falabella.cl>
2023-01-16 11:35:44 -0300

    Se muestra titulos de gifs en la pantalla

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
-    getGifs( category );
- 
+    const [images, setImages] = useState([])
+
+    const getImages = async() => {
+        const newImages = await getGifs( category )
+        setImages( newImages )
+    }
+
+    useEffect( () => {
+        getImages();
+    }, [])
+
+    
     return (
         <>
             <h3>{ category }</h3>
+            
+            <ol>
+                {
+                    images.map(({ id, title }) => {
+                        return <li key={ id }>{ title }</li>
+                    })
+                }
+            </ol>
         </>
     )
 }
